3f011c0abc4888475903315377a9ca623a069396
CloverFeng <sunkesi02@gmail.com>
2019-05-04 22:21:05 +0800

    修改 Implent_SRP 專案內容的提示字顏色

--- SOLID/Implent/Implent_SRP/Program.cs
+++ SOLID/Implent/Implent_SRP/Program.cs
@@ -11,10 +11,12 @@ namespace Implent_SRP
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("MultiTaskWaiter 為一個可以執行多項工作的超好用服務生，");
             Console.WriteLine("可以同時執行內、外場的餐廳服務工作，但如果一直讓他內、外場不停地跑，");
             Console.WriteLine("疲勞值累積會加快，很快就會累倒，");
             Console.WriteLine("這是一個違反單一職責原則的例子。");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("\n========================違反單一職責原則範例========================\n");
 
             var multiTaskWaiter = new UserfulWaiter("店小二");
@@ -26,8 +28,11 @@ namespace Implent_SRP
             multiTaskWaiter.Preparation();
 
             Console.WriteLine("\n========================單一職責原則範例========================\n");
+
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("內、外場職責分離後，員工就不需要一人身兼多職，");
             Console.WriteLine("只需做好份內的事，疲勞度也就不會增加得太快。\n");
+            Console.ForegroundColor = ConsoleColor.White;
 
             var insideWaiter = new InsideWaiter("店小三");
             insideWaiter.Cook();
